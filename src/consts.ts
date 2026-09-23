5ac23f8ac9688d11ae10a219b8f7dd8af4997902
samacuto <samuelct1998@gmail.com>
2026-09-23 19:42:05 +0200

    feat: create Abigail viola landing page

---
+++ src/consts.ts
@@ -0,0 +1,18 @@
+import type { Site, Socials } from "@types";
+
+export const SITE: Site = {
+  TITLE: "abiprian",
+  DESCRIPTION: "Abigail Prián, violista y música.",
+  EMAIL: "abi2viola@gmail.com",
+};
+
+export const SOCIALS: Socials = [
+  {
+    NAME: "X",
+    HREF: "https://x.com/abiprian",
+  },
+  {
+    NAME: "Instagram",
+    HREF: "https://instagram.com/abiprian",
+  },
+];
---
+++ src/types.ts
@@ -0,0 +1,10 @@
+export type Site = {
+  TITLE: string;
+  DESCRIPTION: string;
+  EMAIL: string;
+};
+
+export type Socials = {
+  NAME: string;
+  HREF: string;
+}[];
